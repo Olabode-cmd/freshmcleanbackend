265a401fbd7cf117dff555ef07978e02c7544288
emekadefirst <victorchibuogwu33@gmail.com>
2025-04-18 17:40:30 +0100

    kyc done

--- service/models/kyc_model.py
+++ service/models/kyc_model.py
@@ -2,19 +2,6 @@ from django.db import models
 from user.models import User
 from utils.base_model import BaseModel
 
-
-class KYCDoucment(BaseModel):
-    fullname = models.CharField(max_length=255, unique=True)
-    phone_number = models.CharField(max_length=15, unique=True)
-    id_image = models.ImageField(upload_to="ID image")
-    resume = models.FileField(upload_to="Resume")
-    id_document = models.FileField(upload_to="Resume")
-
-    def save(self, *args, **kwargs):
-        ...
-        return super().save()
-    
-
 class UserKYCProfile(BaseModel):
     class STATUS(models.TextChoices):
         PENDING = 'pending', 'Pending'
@@ -22,7 +9,11 @@ class UserKYCProfile(BaseModel):
         DECLINED = 'declined', 'Declined'
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    document = models.ForeignKey(KYCDoucment, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    id_image = models.ImageField(upload_to="ID image")
+    resume = models.FileField(upload_to="Resume")
+    id_document = models.FileField(upload_to="Resume")
     status = models.CharField(max_length=15, choices=STATUS.choices, default=STATUS.PENDING, blank=True)
 
 
--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .routes.coupon_route import CouponListCreateView, CouponRetrieveUpdateDestroyView
+from .routes.kyc_router import UserKYCProfileListCreateView, UserKYCProfileRetrieveUpdateDestroyView
 from .routes.apartment_route import ApartmentTypeListCreateView, ApartmentTypeRetrieveUpdateDestroyAPIViewView, ExtraSpaceViewListCreate, ExtraSpaceRetrieveUpdateDestroyAPIViewView
 
 urlpatterns = [
@@ -7,10 +8,15 @@ urlpatterns = [
     path('coupons/<uuid:id>/', CouponRetrieveUpdateDestroyView.as_view(), name='retrieve-update-delete-coupon'),
 
     # Apartment
-    path('apartment-types/', ApartmentTypeListCreateView.as_view(), name='create-list-coupon'),
-    path('apartment-types/<uuid:id>/', ApartmentTypeRetrieveUpdateDestroyAPIViewView.as_view(), name='retrieve-update-delete-coupon'),
+    path('apartment-types/', ApartmentTypeListCreateView.as_view(), name='create-list-apartment-type'),
+    path('apartment-types/<uuid:id>/', ApartmentTypeRetrieveUpdateDestroyAPIViewView.as_view(), name='retrieve-update-delete-apartment-type'),
 
     # ExtraSpace
-    path('extra-spaces/', ExtraSpaceViewListCreate.as_view(), name='create-list-coupon'),
-    path('extra-spaces/<uuid:id>/', ExtraSpaceRetrieveUpdateDestroyAPIViewView.as_view(), name='retrieve-update-delete-coupon'),
+    path('extra-spaces/', ExtraSpaceViewListCreate.as_view(), name='create-list-extra-space'),
+    path('extra-spaces/<uuid:id>/', ExtraSpaceRetrieveUpdateDestroyAPIViewView.as_view(), name='retrieve-update-delete-extra-space'),
+
+    #KYC
+    path('user-cleaner-kyc/', UserKYCProfileListCreateView.as_view(), name='create-list-kyc'),
+    path('user-cleaner-kyc/<uuid:id>/', UserKYCProfileRetrieveUpdateDestroyView.as_view(), name='retrieve-update-delete-kyc'),
+
 ]
